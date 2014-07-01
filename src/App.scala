f58f874cccd715ca48f98058770ed80469b1774e
Eustache <eustache@diemert.fr>
2014-07-01 14:54:49 +0200

    modular version

--- src/App.scala
+++ src/App.scala
@@ -5,6 +5,8 @@ import org.apache.spark.mllib.regression.{LabeledPoint, LinearRegressionWithSGD}
 import org.apache.spark.mllib.tree.DecisionTree
 import org.apache.spark.mllib.tree.configuration.Algo
 import org.apache.spark.mllib.tree.impurity.Variance
+import org.apache.spark.mllib.tree.model.DecisionTreeModel
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.SparkContext._
 
@@ -13,47 +15,61 @@ import org.apache.spark.SparkContext._
  */
 object App {
 
-  def main(args : scala.Array[scala.Predef.String]) : scala.Unit = {
-    val conf = new SparkConf()
-      .setAppName("MLLib POC")
-      .setMaster("local[4]")
-      .setSparkHome("/usr/local/Cellar/apache-spark/1.0.0/libexec/")
-    val sc = new SparkContext(conf)
-    val path: String = "YearPredictionMSD.t"
-    println(sc.textFile(path).count())
-    val housingData = LibSVMRegressionLoader.loadLibSVMRegressionFile(
+  def loadLibSVMRegressionDataSets(sc: SparkContext,
+                                  trainingSetPath: String,
+                                  testingSetPath: String): Tuple2[RDD[LabeledPoint], RDD[LabeledPoint]] = {
+    val trainDataRDD = LibSVMRegressionLoader.loadLibSVMRegressionFile(
       sc,
-      path,
+      trainingSetPath,
       -1,
       10)
-    housingData.cache()
-//    val example = housingData.take(1)(0)
-//    println(example.label+" - "+example.features)
+    val testDataRDD = LibSVMRegressionLoader.loadLibSVMRegressionFile(
+      sc,
+      testingSetPath,
+      -1,
+      10)
+    (trainDataRDD, testDataRDD)
+  }
 
-    // Split data into training and test.
-    val splits = housingData.randomSplit(Array(0.8, 0.2), seed = 42L)
-    val training = splits(0)
-    val test = splits(1)
-
-    // Run training algorithm to build the model
-    val model = DecisionTree.train(training, Algo.Regression, Variance, 15)
-
-    // Compute raw scores on the test set.
-    val valuesAndPreds = housingData.map { point: LabeledPoint =>
-      val prediction = model.predict(point.features)
-      (point.label, prediction)
-    }
-
-//    valuesAndPreds.take(100).map{case(v, p) => println(v+" / "+p+" = "+(v-p)+" => "+math.pow((v - p), 2))}
-
-    // Get evaluation metrics.
+  def evaluate(valuesAndPreds: RDD[Tuple2[Double, Double]]) = {
     val meanError = valuesAndPreds.map{case(v, p) => v - p}.mean()
-    println("Mean Error = " + MSE)
+    println("Mean Error = " + meanError)
+    val squareRootedMeanError = valuesAndPreds.map{case(v, p) => Math.sqrt(v - p)}.mean()
+    println("Square Root Mean Error = " + squareRootedMeanError)
     val MSE = valuesAndPreds.map{case(v, p) => math.pow((1e-9 + v - p), 2)}.mean()
-    println("training Mean Squared Error = " + MSE)
-    val MSE = valuesAndPreds.map{case(v, p) => math.pow((1e-9 + v - p), 2)}.mean()
-    println("training Mean Squared Error = " + MSE)
+    println("Mean Squared Error = " + MSE)
+  }
 
+  def initializeLocalSparkContext() = {
+    val conf = new SparkConf()
+      .setAppName("MLLib POC")
+      .setMaster("local[1]") // set 4 local executors
+      .setSparkHome("/usr/local/Cellar/apache-spark/1.0.0/libexec/")
+      .set("spark.executor.memory", "1g")
+      .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    val sc = new SparkContext(conf)
+    sc
+  }
+
+  def predict(testingData: RDD[LabeledPoint], model: DecisionTreeModel): RDD[Tuple2[Double, Double]] = {
+    testingData.map { point: LabeledPoint =>
+      val prediction = model.predict(point.features)
+      Tuple2[Double, Double](point.label, prediction.asInstanceOf[Double])
+    }
+  }
+
+  def main(args : scala.Array[scala.Predef.String]) : scala.Unit = {
+
+    val sc = initializeLocalSparkContext()
+
+    val (trainingData, testingData): (RDD[LabeledPoint], RDD[LabeledPoint]) =
+      loadLibSVMRegressionDataSets(sc, "YearPredictionMSD", "YearPredictionMSD.t")
+
+    val model = DecisionTree.train(trainingData, Algo.Regression, Variance, 5)
+
+    val valuesAndPredictions = predict(testingData, model)
+
+    evaluate(valuesAndPredictions)
   }
 
 }
