148acdb72d7cb224d7bee4d941642012ab588ccf
Eustache <eustache@diemert.fr>
2014-07-03 14:49:04 +0200

    ridge regression (not working atm)

--- src/App.scala
+++ src/App.scala
@@ -1,7 +1,6 @@
 package com.tedemis.sandbox
 
-import org.apache.spark.mllib.classification.LogisticRegressionWithSGD
-import org.apache.spark.mllib.regression.{LabeledPoint, LinearRegressionWithSGD}
+import org.apache.spark.mllib.regression._
 import org.apache.spark.mllib.tree.DecisionTree
 import org.apache.spark.mllib.tree.configuration.Algo
 import org.apache.spark.mllib.tree.impurity.Variance
@@ -16,34 +15,40 @@ import org.apache.spark.SparkContext._
 object App {
 
   def loadLibSVMRegressionDataSets(sc: SparkContext,
-                                  trainingSetPath: String,
-                                  testingSetPath: String): Tuple2[RDD[LabeledPoint], RDD[LabeledPoint]] = {
+                                   trainingSetPath: String,
+                                   testingSetPath: String): Tuple2[RDD[LabeledPoint], RDD[LabeledPoint]] = {
     val trainDataRDD = LibSVMRegressionLoader.loadLibSVMRegressionFile(
       sc,
       trainingSetPath,
       -1,
-      10)
+      30)
     val testDataRDD = LibSVMRegressionLoader.loadLibSVMRegressionFile(
       sc,
       testingSetPath,
       -1,
-      10)
+      30)
     (trainDataRDD, testDataRDD)
   }
 
   def evaluate(valuesAndPreds: RDD[Tuple2[Double, Double]]) = {
-    val meanError = valuesAndPreds.map{case(v, p) => v - p}.mean()
-    println("Mean Error = " + meanError)
-    val squareRootedMeanError = valuesAndPreds.map{case(v, p) => Math.sqrt(v - p)}.mean()
-    println("Square Root Mean Error = " + squareRootedMeanError)
-    val MSE = valuesAndPreds.map{case(v, p) => math.pow((1e-9 + v - p), 2)}.mean()
+    println("XOXOX")
+    valuesAndPreds.take(10).map(t => println(t))
+    val absoluteErrors = valuesAndPreds.map{case(v, p) => Math.abs(v - p)}.cache()
+    val stableAbsoluteErrors = absoluteErrors.filter(e => !e.isNaN)
+    println(stableAbsoluteErrors.count()+" stable predictions out of "+absoluteErrors.count())
+    val meanError = stableAbsoluteErrors.mean()
+    val stdError = stableAbsoluteErrors.stdev()
+    println("Mean Error = " + meanError + " +/- " + stdError)
+    val MSE = stableAbsoluteErrors.map{e => math.pow(e, 2)}.mean()
     println("Mean Squared Error = " + MSE)
+    val RMSE = Math.sqrt(MSE)
+    println("Root Mean Squared Error = " + RMSE)
   }
 
   def initializeLocalSparkContext() = {
     val conf = new SparkConf()
       .setAppName("MLLib POC")
-      .setMaster("local[1]") // set 4 local executors
+      .setMaster("local[2]")
       .setSparkHome("/usr/local/Cellar/apache-spark/1.0.0/libexec/")
       .set("spark.executor.memory", "1g")
       .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
@@ -51,7 +56,8 @@ object App {
     sc
   }
 
-  def predict(testingData: RDD[LabeledPoint], model: DecisionTreeModel): RDD[Tuple2[Double, Double]] = {
+//  def predict(testingData: RDD[LabeledPoint], model: DecisionTreeModel): RDD[Tuple2[Double, Double]] = {
+  def predict(testingData: RDD[LabeledPoint], model: RidgeRegressionModel): RDD[Tuple2[Double, Double]] = {
     testingData.map { point: LabeledPoint =>
       val prediction = model.predict(point.features)
       Tuple2[Double, Double](point.label, prediction.asInstanceOf[Double])
@@ -65,9 +71,11 @@ object App {
     val (trainingData, testingData): (RDD[LabeledPoint], RDD[LabeledPoint]) =
       loadLibSVMRegressionDataSets(sc, "YearPredictionMSD", "YearPredictionMSD.t")
 
-    val model = DecisionTree.train(trainingData, Algo.Regression, Variance, 5)
+//    val model = DecisionTree.train(trainingData, Algo.Regression, Variance, 10)
+    
+    val trainedModel = RidgeRegressionWithSGD.train(trainingData, 1000, 10, 1)
 
-    val valuesAndPredictions = predict(testingData, model)
+    val valuesAndPredictions = predict(testingData, trainedModel)
 
     evaluate(valuesAndPredictions)
   }
